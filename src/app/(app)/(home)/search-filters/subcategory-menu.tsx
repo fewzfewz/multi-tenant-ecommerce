13d6a0da70031cf8c7aa5a8b1018f67882a1ef45
fewzfewz <fewzfewzan@gmail.com>
2025-09-22 09:46:30 +0300

    category finalization

--- src/app/(app)/(home)/search-filters/subcategory-menu.tsx
+++ src/app/(app)/(home)/search-filters/subcategory-menu.tsx
@@ -1,8 +1,9 @@
-import { Category } from "@/payload-types";
+
 import Link from "next/link";
+import { CustomCategory } from "../types";
 
 interface Props{
-    category: Category;
+    category: CustomCategory;
     isOpen: boolean;
     position: { top: number; left: number; }
 }
@@ -54,7 +55,7 @@ export const SubcategoryMenu = ({category, isOpen, position}: Props) => {
           return (
             <Link
               key={subcategory.slug}
-              href={`/category/${subcategory.slug}`}
+              href={`/${category.slug}/${subcategory.slug}`}
               className="w-full text-left p-4 hover:bg-black hover:text-white flex justify-between items-center underline font-medium"
             >
               {subcategory.name}
